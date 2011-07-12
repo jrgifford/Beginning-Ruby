b7ad35fc6c0c5737b590ef291eac71788fa2a7b8
James Gifford <james@jamesrgifford.com>
2011-07-11 20:36:36 -0400

    Made it more

--- count-lines.rb
+++ count-lines.rb
@@ -1,3 +1,8 @@
+text=''
 line_count = 0
-File.open("./skype-editorial-1.txt").each { |line| line_count += 1 }
-puts line_count
+File.open("./skype-editorial-1.txt").each do |line|
+  line_count += 1
+  text << line 
+end
+
+puts "#{line_count} lines"
