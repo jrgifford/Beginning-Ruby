15c2fea4d90200a812981e39af4433a45dcaffa1
James Gifford <james@jamesrgifford.com>
2011-07-11 21:00:26 -0400

    Added more stuff to count lines - i'm almost done as soon as I get the file name printed

--- count-lines.rb
+++ count-lines.rb
@@ -1,8 +1,18 @@
-text=''
-line_count = 0
-File.open("./skype-editorial-1.txt").each do |line|
-  line_count += 1
-  text << line 
-end
+lines = File.readlines("./skype-bought-by-microsoft.txt")
+line_count = lines.size
+text = lines.join
+word_count = text.split.length
+character_count = text.length
+character_count_nospaces = text.gsub(/\s+/, ' ').length
+paragraph_count = text.split(/\n\n/).length
+setence_count = text.split(/\.|\?|!/).length
 
 puts "#{line_count} lines"
+puts "#{character_count} characters"
+puts "#{character_count_nospaces} characters excluding spaces"
+puts "#{word_count} words"
+puts "#{paragraph_count} paragraphs"
+puts "#{setence_count} setences"
+puts "#{setence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{word_count / setence_count} words per setence (average)"
+puts "#{Files.readlines}"
