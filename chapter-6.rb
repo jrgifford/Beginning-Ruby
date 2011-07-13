e87f90e4940aba1bbe0735f248a06b5792804c1c
James Gifford <james@jamesrgifford.com>
2011-07-12 22:00:25 -0400

    Moving on to chapter 6

---
+++ chapter-6.rb
@@ -0,0 +1,35 @@
+class Shape
+end
+
+class Square < Shape
+  def initialize(side_length)
+    @side_length = side_length
+  end
+
+  def area
+    @side_length * side_length
+  end
+
+  def perimeter
+    @side_length * 4
+  end
+end
+
+class Triangle < Shape
+  def initialize(base_width, height, side1, side2, side3)
+    @base_width = base_width
+    @height = height
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    end
+
+  def area
+    @base_width * @height / 2
+  end
+
+  def perimeter
+    @side1 + @side2 + @side3
+  end
+end
+
--- count-lines.rb
+++ count-lines.rb
@@ -5,13 +5,13 @@ word_count = text.split.length
 character_count = text.length
 character_count_nospaces = text.gsub(/\s+/, ' ').length
 paragraph_count = text.split(/\n\n/).length
-setence_count = text.split(/\.|\?|!/).length
+sentence_count = text.split(/\.|\?|!/).length
 
 puts "#{line_count} lines"
 puts "#{character_count} characters"
 puts "#{character_count_nospaces} characters excluding spaces"
 puts "#{word_count} words"
 puts "#{paragraph_count} paragraphs"
-puts "#{setence_count} setences"
-puts "#{setence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{sentence_count} setences"
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
 puts "#{word_count / setence_count} words per setence (average)"
