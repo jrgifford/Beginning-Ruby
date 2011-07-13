43c71707e2c1388840296ad703b99476b9f34d01
James Gifford <james@jamesrgifford.com>
2011-07-12 22:24:32 -0400

    Created the stuff in page 122

---
+++ chapter-6-page-122.rb
@@ -0,0 +1,14 @@
+class Square
+  def initialize
+    if defined?(@@number_of_squares)
+      @@number_of_squares += 1
+    else
+      @@number_of_squares = 1
+    end
+  end
+  
+  def self.count
+    @@number_of_squares
+  end
+end
+
