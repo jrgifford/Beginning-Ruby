c37f582ea6ca70804731d9084c996feb12f0b1ae
James Gifford <james@jamesrgifford.com>
2011-07-28 11:29:06 -0400

    Working through chapter 7

--- completed-code-by-me/chapter-7/b.rb
+++ completed-code-by-me/chapter-7/b.rb
@@ -1,4 +1,5 @@
-require 'a'
+# Having to use load instead of require for some weird reason - about to check the errata from appress
+load 'a.rb'
 puts "Hello from b.rb"
-require 'a'
+load 'a.rb'
 puts "Hello again from b.rb"
