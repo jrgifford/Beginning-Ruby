3959c1ee6dc6dba943119bcfb1c12e281a328ee9
James Gifford <james@jamesrgifford.com>
2011-08-13 11:39:19 -0400

    Working at this again... maybe?

--- completed-code-by-me/chapter-13/blog/config/initializers/secret_token.rb
+++ completed-code-by-me/chapter-13/blog/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'ed1aa7687e0b1dd5ff1c37fdbcd008e8eca7ddbbaa750a037bb0de92702fb8bb5cf3f374637f8a219cfc7283277d847d6f6055fe3c154353afc90e978b3a5156'
+Blog::Application.config.secret_token = '64855a476058ac9ffe9f9c65f2ad0f30d993b99f18a2682805ecc6cafeabccb673400a6fa56928660a09c3dc6948b6a4b2e8c584674612d261a4e10cf5e27eb2'
--- completed-code-by-me/chapter-13/blog/config/routes.rb
+++ completed-code-by-me/chapter-13/blog/config/routes.rb
@@ -1,9 +1,4 @@
 Blog::Application.routes.draw do
-  resources :posts
-
-  get "home/index"
-
-  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -53,6 +48,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
