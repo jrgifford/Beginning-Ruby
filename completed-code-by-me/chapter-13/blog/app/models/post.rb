6a0d53a1b4f257ee8a131ea375b3d66c37f78c55
James Gifford <james@jamesrgifford.com>
2011-08-13 14:56:17 -0400

    Authed is go

--- completed-code-by-me/chapter-13/blog/app/models/post.rb
+++ completed-code-by-me/chapter-13/blog/app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
- 
-  has_many :comments
+  has_many :comments, :dependent => :destroy
 end
+
