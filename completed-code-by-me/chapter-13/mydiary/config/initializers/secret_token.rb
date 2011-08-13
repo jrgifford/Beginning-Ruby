d49eed0584f4c208ffacd9473d48da9e65bd3c4c
James Gifford <james@jamesrgifford.com>
2011-08-13 09:06:08 -0400

    Going to ignore the rails stuff

--- completed-code-by-me/chapter-13/mydiary/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mydiary::Application.config.secret_token = '71f31fb37ab6cd2577760e24394326f672729e28d04242029fda907e70e151f73fcb579b6d7f6f0996d62a6559972d6ce64ecc0602c9cb9b60559ab3d3e41b74'
